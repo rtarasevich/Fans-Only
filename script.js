3c943e7751fcdd0981d34078a839d9ac360cc74b
Taryn Moore <tarynmoore628@gmail.com>
2022-06-07 14:03:59 -0600

    got the user search to render with the youtube api and the event listener to work, added comments throughout

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 var buttonEl = document.querySelector("#btn")
 var userSearchEl = document.querySelector("#userSearch");
 
-// buttonEl.addEventListener('click', results.html);
-// function
+var redirect = function(userChoice){
+    var redirectPage = buttonEl.value;
+    var queryString = '?artist=' + userChoice;
+    var uri = redirectPage + queryString;
 
-var userInformation = function(event) {
+    window.location = uri;
+
+    // displayYoutube();
+}
+
+var userInformation = function(event){
     event.preventDefault();
-
     var userChoice = userSearchEl.value
-    console.log(userChoice);
+    localStorage.setItem("userChoice" , userChoice);
+    // console.log("did this store", localStorage.setItem("userChoice" , userChoice)) 
+    // debugger;
 
+    if(userChoice != "") {
+        redirect(userChoice);
+    }
 }
 
+buttonEl.addEventListener("click", userInformation);
 
-var displayYoutube = function(userChoice) {
-    var youtubeApi = "https://www.googleapis.com/youtube/v3/search=" + userSearchEl + "part=snippet.thumbnails.default&chart=mostPopular&videoCategoryId=snippet&key=AIzaSyCJVNary6yBhD7_VSyVCxAaleA0ZeyW-Vw"
 
-    fetch(youtubeApi)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(displayYoutube){
 
-    })
-    console.log(youtubeApi);
-}
 
-// var youtubeApi = "https://www.googleapis.com/youtube/v3/videos?part=snippet.thumbnails.default&chart=mostPopular&videoCategoryId=snippet&key=AIzaSyCJVNary6yBhD7_VSyVCxAaleA0ZeyW-Vw"
-// console.log(youtubeApi);
